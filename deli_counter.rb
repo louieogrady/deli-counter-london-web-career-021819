589df87343ee45d1b8e0ed845aeb8328a0560def
Louie O'Grady <l.ogrady@gmail.com>
2019-02-01 08:49:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,24 @@
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli == []
+def now_serving(array)
+  if array == []
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.shift}."
+    puts "Currently serving #{array.shift}."
   end
-
 end
 
-def line(katz_deli)
-  if katz_deli == []
+def line(array)
+  if array == []
     puts "The line is currently empty."
   else
     output = "The line is currently:"
-    katz_deli.each_with_index do |name, position|
+    array.each_with_index do |name, position|
       output += " #{position+1}. #{name}"
     end
     puts "#{output}"
   end
-
-
 end
