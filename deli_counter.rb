d4626879ea44d6ca5cfd2ba2825a212e416c66b4
Louie O'Grady <l.ogrady@gmail.com>
2019-02-01 08:50:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,15 @@
+def line(array)
+  if array == []
+    puts "The line is currently empty."
+  else
+    output = "The line is currently:"
+    array.each_with_index do |name, position|
+      output += " #{position+1}. #{name}"
+    end
+    puts "#{output}"
+  end
+end
+
 def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
@@ -11,14 +23,3 @@ def now_serving(array)
   end
 end
 
-def line(array)
-  if array == []
-    puts "The line is currently empty."
-  else
-    output = "The line is currently:"
-    array.each_with_index do |name, position|
-      output += " #{position+1}. #{name}"
-    end
-    puts "#{output}"
-  end
-end
